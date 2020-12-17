81ed634b672ca3048ef5f849fa3d1826e10492f2
William Shea <shea_william@outlook.com>
2020-12-17 15:31:43 +0100

    update

--- upload.php
+++ upload.php
@@ -3,7 +3,6 @@
 var_dump($_FILES);
 
 foreach($_FILES["myFiles"]["tmp_name"] as $key => $value){
-    // $targetPath = "uploads/" . basename($_FILES["myFiles"]["name"][$key]);
     $targetPath = "active/" . $_POST["dataName"];
     move_uploaded_file($value, $targetPath);
 }
