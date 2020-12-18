ee61a9104723242b1f38c7b91d01ad00301df160
William Shea <shea_william@outlook.com>
2020-12-18 11:16:37 +0100

    update

--- adventskalender.js
+++ adventskalender.js
@@ -1,30 +1,126 @@
+const calendarContainer = document.querySelector(".container");
 
-    // var date = new Date();
+const calendarDays = 24;
 
-    // var day = date.getDate();
-    // var month = date.getMonth() + 1;
-    // var year = date.getFullYear();
+const openDoor = (path, number, event) => {
+    var dateUser = new Date(document.getElementById("dateSelection").value);
+    if(dateUser.getMonth() == 11 && dateUser.getDate() >= number){
+        event.target.parentNode.style.backgroundImage = `url(${path})`;
+        event.target.style.opacity = "0";
+        event.target.style.backgroundColor = "#222222";
+    }
+}
+
+const createCalendar = () => {
+    for(var i = 0; i  < calendarDays; i++) {
+        const calendarDoor = document.createElement("div");
+        const calendarDoorText = document.createElement("div");
+
+        calendarDoor.classList.add("image");
+        calendarDoor.style.gridArea = "door" + (i + 1);
+        calendarContainer.appendChild(calendarDoor);
+
+        calendarDoorText.classList.add("text");
+        calendarDoorText.innerHTML = i + 1;
+        calendarDoor.appendChild(calendarDoorText);
+
+        dayNumber = i + 1;
+        // var loginName = "active";
+        var picturePath = `./active/${dayNumber}.jpg`;
+        if(dayNumber < 10){
+            dayNumber = "0" + dayNumber;
+        }
+        calendarDoorText.addEventListener("click", openDoor.bind(null,  picturePath, dayNumber));
+    }
+}
+
+function makeArray(w, h, val) {
+    var arr = [];
+    for(let i = 0; i < h; i++) {
+        arr[i] = [];
+        for(let j = 0; j < w; j++) {
+            arr[i][j] = val;
+        }
+    }
+    return arr;
+}
+
+var test = makeArray(4,6, null);
+
+for(var i = 0 ; i < 6; i++){
+    for(var a = 0; a < 4; a++){
+        var getOut = true;
+        do{
+            var tmp = Math.floor(Math.random()*24+1);
+            for(var z = 0; z <= i; z++){
+                if(test[z].includes(tmp)){
+                    getOut = test[z].includes(tmp);
+                    break;
+                }else{
+                    getOut = test[z].includes(tmp);
+                }
+            }
+            if (!getOut){ //nicht true = befüllen
+                test[i][a] = tmp;
+            }
+        }
+        while (getOut);
+    }
     
-    // if (month < 10) month = "0" + month;
-    // if (day < 10) day = "0" + day;
-    
-    // var today = year + "-" + month + "-" + day;       
-    // document.getElementById("dateSelection").value = today;
-    
-    // console.log("test");
+}
 
-function changePicture(){
+for(var i = 0; i < test.length; i++){
+    var txtTmp= "";
+    for(var a = 0; a < test[i].length; a++){
+        if(a == 0){
+            txtTmp += "door" + test[i][a];
+        }else{
+            txtTmp += " " + "door" + test[i][a];
+        }
+    }
+    // console.log(txtTmp);
+}
+// console.log(test);
 
-    document.getElementById("picture1").src="active/img01.png";
-    console.log("test");
-};
+function changeOrder() {
+    var elem= document.getElementById("container");
+    var txtTmp = [];
+    for(var i = 0; i < test.length; i++){
+        for(var a = 0; a < test[i].length; a++){
+            if(a == 0){
+                txtTmp[i] = "door" + test[i][a];
+            }else{
+                txtTmp[i] += " " + "door" + test[i][a];
+            }
+        }
+        // console.log(txtTmp[i]);
+    }
 
-function changeCSS() {
-    console.log(document.getElementById("picture1").className);
-    document.getElementById("picture1").className = "door-box door-img-new-2";
-  }
+	elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
+}
 
-  function changeCSS2() {
-    console.log(document.getElementById("picture1").className);
-    document.getElementById("picture21").className = "door-box door-img-new-24";
-  }
+
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();  
+if (month < 10){
+    month = "0" + month;
+}
+if (day < 10){
+    day = "0" + day;
+}
+var today = year + "-" + month + "-" + day;       
+document.getElementById("dateSelection").value = today;
+
+
+function openDialog(){
+    document.getElementById('uploadDialog').showModal();
+}
+
+function closeDialog(){
+    document.getElementById('uploadDialog').close();
+}
+
+window.addEventListener("load", createCalendar);
+window.addEventListener("load", changeOrder);
