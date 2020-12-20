b758b818cb07f7bb756be3af81ed2160e9c15256
William Shea <shea_william@outlook.com>
2020-12-20 23:36:31 +0100

    update

--- adventskalender.js
+++ adventskalender.js
@@ -1,14 +1,3 @@
-// leeres Array erstellen
-function makeArray(w, h, val) {
-    var arr = [];
-    for(let i = 0; i < h; i++) {
-        arr[i] = [];
-        for(let j = 0; j < w; j++) {
-            arr[i][j] = val;
-        }
-    }
-    return arr;
-}
 // Konstante mit den 24 Tagen
 const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 //Neuer Shuffle-Algorithmus
@@ -35,6 +24,45 @@ if (day < 10){
 }
 var today = year + "-" + month + "-" + day;       
 document.getElementById("dateSelection").value = today;
+
+//Funktion zum Layout angeben + anpassen
+function BrauchtNochEinenNamen(){
+
+    //Check ob Landscape oder Portrait
+    if(window.innerHeight > window.innerWidth){
+        width = 4;
+    }else{
+        width = 6;
+    }
+
+    //Tage werden auf das Layout passend in ein 2D-Array aufgeteilt
+    var tmparray = [];
+    var j = 0;
+    for(var i = 0;i < days.length; i += width){
+        tmparray[j] = days.slice(i,i+width);
+        j++;
+    }
+
+    //String-Konvertierung + Layout eintragen
+    var elem= document.getElementById("container");
+    var txtTmp = [];
+    for(var i = 0; i < tmparray.length; i++){
+        for(var a = 0; a < tmparray[i].length; a++){
+            if(a == 0){
+                txtTmp[i] = "door" + tmparray[i][a];
+            }else{
+                txtTmp[i] += " " + "door" + tmparray[i][a];
+            }
+        }
+    }
+
+    //Layout eintragen
+    if(width == 4){
+        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
+    }else{
+        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}"`;
+    }
+}
 
 //Funktion zum Türen öffnen
 const openDoor = (path, number, event) => {
@@ -69,147 +97,12 @@ const createCalendar = () => {
         calendarDoorText.addEventListener("click", openDoor.bind(null,  picturePath, dayNumber));
     }
 
-    if(window.innerHeight > window.innerWidth){
-        width = 4;
-    }else{
-        width = 6;
-    }
-
-// var width = 6;
-shuffleArray(days);
-var tmparray = [];
-var j = 0;
-//Arraysplittung
-for(var i = 0;i < days.length; i += width){
-    tmparray[j] = days.slice(i,i+width);
-    j++;
+    shuffleArray(days);
+    BrauchtNochEinenNamen();
 }
-//String-Konvertierung + Layout eintragen
-    var elem= document.getElementById("container");
-    var txtTmp = [];
-    for(var i = 0; i < tmparray.length; i++){
-        for(var a = 0; a < tmparray[i].length; a++){
-            if(a == 0){
-                txtTmp[i] = "door" + tmparray[i][a];
-            }else{
-                txtTmp[i] += " " + "door" + tmparray[i][a];
-            }
-        }
-    }
-if(width == 4){
-    elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
-}else{
-    elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}"`;
-}
-}
-
 
 //Kalender kreieren
 window.addEventListener("load", createCalendar);
 
-function test() {
-    if(window.innerHeight > window.innerWidth){
-        width = 4;
-    }else{
-        width = 6;
-    }
-    
-    // var width = 6;
-    var tmparray = [];
-    var j = 0;
-    //Arraysplittung
-    for(var i = 0;i < days.length; i += width){
-        tmparray[j] = days.slice(i,i+width);
-        console.log(tmparray[j]);
-        j++;
-    }
-    //String-Konvertierung + Layout eintragen
-        var elem= document.getElementById("container");
-        var txtTmp = [];
-        for(var i = 0; i < tmparray.length; i++){
-            for(var a = 0; a < tmparray[i].length; a++){
-                if(a == 0){
-                    txtTmp[i] = "door" + tmparray[i][a];
-                }else{
-                    txtTmp[i] += " " + "door" + tmparray[i][a];
-                }
-            }
-        }
-    if(width == 4){
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
-    }else{
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}"`;
-    }
-}
-
-window.addEventListener('resize', function(event){
-    if(window.innerHeight > window.innerWidth){
-        width = 4;
-    }else{
-        width = 6;
-    }
-    
-    // var width = 6;
-    var tmparray = [];
-    var j = 0;
-    //Arraysplittung
-    for(var i = 0;i < days.length; i += width){
-        tmparray[j] = days.slice(i,i+width);
-        console.log(tmparray[j]);
-        j++;
-    }
-    //String-Konvertierung + Layout eintragen
-        var elem= document.getElementById("container");
-        var txtTmp = [];
-        for(var i = 0; i < tmparray.length; i++){
-            for(var a = 0; a < tmparray[i].length; a++){
-                if(a == 0){
-                    txtTmp[i] = "door" + tmparray[i][a];
-                }else{
-                    txtTmp[i] += " " + "door" + tmparray[i][a];
-                }
-            }
-        }
-    if(width == 4){
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
-    }else{
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}"`;
-    }
-});
-
-//Mobile-Screenrotation
-window.addEventListener("orientationchange", function(event) {
-    var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
-    if (orientation.substr(0,9) ==  "landscape"){
-        width = 6;
-    }else{
-        width = 4;
-    }
-    
-    // var width = 6;
-    var tmparray = [];
-    var j = 0;
-    //Arraysplittung
-    for(var i = 0;i < days.length; i += width){
-        tmparray[j] = days.slice(i,i+width);
-        console.log(tmparray[j]);
-        j++;
-    }
-    //String-Konvertierung + Layout eintragen
-        var elem= document.getElementById("container");
-        var txtTmp = [];
-        for(var i = 0; i < tmparray.length; i++){
-            for(var a = 0; a < tmparray[i].length; a++){
-                if(a == 0){
-                    txtTmp[i] = "door" + tmparray[i][a];
-                }else{
-                    txtTmp[i] += " " + "door" + tmparray[i][a];
-                }
-            }
-        }
-    if(width == 4){
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}" "${txtTmp[4]}" "${txtTmp[5]}"`;
-    }else{
-        elem.style.gridTemplateAreas = `"${txtTmp[0]}" "${txtTmp[1]}" "${txtTmp[2]}" "${txtTmp[3]}"`;
-    }
-  });
+//Listener auf Fenstergröße
+window.addEventListener('resize', BrauchtNochEinenNamen);
